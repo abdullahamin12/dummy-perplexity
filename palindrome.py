74dcc8b8010e6e47ee8acf1c18ef3b1e3e022586
abdullahamin12 <abdullahamin12570@gmail.com>
2026-06-28 22:00:46 +0500

    fix:palindrome test

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,21 @@
 def check_palindrome(string):
-    if string==string[::-1]:
-        return True
-    return False
-print(check_palindrome("madaam"))
+    left=0
+    right=len(string)-1
+    status=True
+    while(left<right):
+        if not string[left].isalnum():
+            left+=1
+            continue
+        if not string[right].isalnum():
+            right-=1
+            continue
+        if string[left].lower()==string[right].lower():
+            status=True
+            left+=1
+            right-=1    
+        else:
+            status=False
+            break
+    return status
+
+print(check_palindrome("aba"))
